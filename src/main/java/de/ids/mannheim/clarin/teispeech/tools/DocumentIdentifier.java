97143518e65992f64c26a4dd275ddca1dade4a02
Bernhard Fisseni <bernhard.fisseni@mail.de>
2018-11-12 14:17:34 +0100

    Provider for @xml:id corrected

--- src/main/java/de/ids/mannheim/clarin/teispeech/tools/DocumentIdentifier.java
+++ src/main/java/de/ids/mannheim/clarin/teispeech/tools/DocumentIdentifier.java
@@ -1,12 +1,17 @@
 package de.ids.mannheim.clarin.teispeech.tools;
 
-import de.ids.mannheim.clarin.teispeech.data.NameSpaces;
-import net.sf.saxon.lib.NamespaceConstant;
-import net.sf.saxon.om.NameChecker;
-import net.sf.saxon.xpath.XPathFactoryImpl;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.apache.commons.lang3.StringUtils;
 import org.jdom2.JDOMException;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.korpora.useful.Utilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,13 +20,9 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import javax.xml.xpath.*;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
-import static net.sf.saxon.lib.NamespaceConstant.OBJECT_MODEL_SAXON;
+import de.ids.mannheim.clarin.teispeech.data.NameSpaces;
+import net.sf.saxon.om.NameChecker;
+import net.sf.saxon.xpath.XPathFactoryImpl;
 
 /**
  * provide IDs für Elements that have none – and remove them
@@ -36,27 +37,28 @@ public class DocumentIdentifier {
     private final static Logger LOGGER = LoggerFactory
             .getLogger(DocumentIdentifier.class.getName());
 
-    public DocumentIdentifier(Document doc){
+    public DocumentIdentifier(Document doc) {
         this.doc = doc;
     }
 
-    public void makeID (Element el) {
-        Supplier<String> make = () -> PREFIX + "_" + (lastId ++);
+    public void makeID(Element el) {
+        Supplier<String> make = () -> PREFIX + "_" + (lastId++);
         String candidate = make.get();
-        while (IDs.contains(candidate)){
+        while (IDs.contains(candidate)) {
             candidate = make.get();
         }
+        IDs.add(candidate);
         NameChecker.isValidNCName(candidate);
         Node n = el.getAttributeNode("id");
         if (n == null || NameSpaces.XML_NS.equals(n.getNamespaceURI())) {
-            el.setAttributeNS("http://www.w3.org/XML/1998/namespace",
-                    "id", candidate);
+            el.setAttributeNS("http://www.w3.org/XML/1998/namespace", "id",
+                    candidate);
         } else {
             LOGGER.warn("skipped ID attribute with wrong namespace");
         }
     }
 
-    public Document getDocument(){
+    public Document getDocument() {
         return doc;
     }
 
@@ -64,16 +66,15 @@ public class DocumentIdentifier {
         System.setProperty("javax.xml.transform.TransformerFactory",
                 "net.sf.saxon.TransformerFactoryImpl");
         try {
-            XPath  xpf = new XPathFactoryImpl().newXPath();
+            XPath xpf = new XPathFactoryImpl().newXPath();
             // note that Java is confused about @xml:id
-            NodeList IDNodes = (NodeList) xpf.compile("//*[@id]")
-                    .evaluate(doc, XPathConstants.NODESET);
+            NodeList IDNodes = (NodeList) xpf.compile("//*[@id]").evaluate(doc,
+                    XPathConstants.NODESET);
             IDs = Utilities.toElementStream(IDNodes).map(e -> {
                 String n = e.getAttributeNode("id").getNamespaceURI();
                 assert n == null || NameSpaces.XML_NS.equals(n);
                 return e.getAttribute("id");
-            })
-                    .collect(Collectors.toSet());
+            }).collect(Collectors.toSet());
             System.err.println(IDs);
             XPath xPath = XPathFactory.newInstance().newXPath();
             // note that Java is confused about @xml:id
@@ -87,13 +88,12 @@ public class DocumentIdentifier {
         return this;
     }
 
-
-    public static Document makeIDs (Document doc) {
+    public static Document makeIDs(Document doc) {
         DocumentIdentifier di = new DocumentIdentifier(doc);
         return di.makeIDs().getDocument();
     }
 
-    public static Document makeIDs (org.jdom2.Document jdoc) {
+    public static Document makeIDs(org.jdom2.Document jdoc) {
         try {
             DocumentIdentifier di = new DocumentIdentifier(
                     Utilities.convertJDOMToDOM(jdoc));
@@ -103,21 +103,21 @@ public class DocumentIdentifier {
         }
     }
 
-    public static org.jdom2.Document jmakeIDs (org.jdom2.Document jdoc) {
+    public static org.jdom2.Document jmakeIDs(org.jdom2.Document jdoc) {
         return Utilities.convertDOMtoJDOM(makeIDs(jdoc));
     }
 
-    public static void removeIDs(Document doc){
+    public static void removeIDs(Document doc) {
         System.setProperty("javax.xml.transform.TransformerFactory",
                 "net.sf.saxon.TransformerFactoryImpl");
         try {
             XPath xp = new XPathFactoryImpl().newXPath();
-            NodeList identified = (NodeList) xp.compile("//*[@id]").
-                        evaluate(doc, XPathConstants.NODESET);
+            NodeList identified = (NodeList) xp.compile("//*[@id]")
+                    .evaluate(doc, XPathConstants.NODESET);
             Utilities.toElementStream(identified).forEach(e -> {
-                if (StringUtils.startsWith(e.getAttributeNS(
-                        "http://www.w3.org/XML/1998/namespace",
-                        "xml:id"),
+                if (StringUtils.startsWith(
+                        e.getAttributeNS("http://www.w3.org/XML/1998/namespace",
+                                "xml:id"),
                         PREFIX)) {
                     e.removeAttribute("xml:id");
                 }
