478680ee2d45b84ee416e511e6f531cc13a89046
Bernhard Fisseni <bernhard.fisseni@mail.de>
2019-04-14 09:36:22 +0200

    small scripts added

---
+++ solo-par.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+let i=0
+FILE=${1%.txt}
+./spindel.sh text2iso --input=$1 --output=${FILE}-0-text2iso.xml --indent
+step=text2iso
+inc_i(){
+    LAST_STEP=${step}
+    last_i=${i}
+    let i+=1
+}
+inc_i
+for step in segmentize guess normalize pos identify unidentify align
+do
+    ./spindel.sh ${step} --input=${FILE}-${last_i}-${LAST_STEP}.xml \
+        --indent --output=${FILE}-${i}-${step}.xml --use-graphs --time 43
+    inc_i
+done
